17d4f74dcae2ac821be6b24a99997068d9a7edde
kh-mo <momkdh2@naver.com>
2019-07-09 19:36:39 +0900

    label 추가

--- check_dataset.py
+++ check_dataset.py
@@ -7,40 +7,45 @@ import torchvision
 import torch.utils.data
 
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
 class mytrainset:
-    train_list = [
-        'data_batch_1',
-        'data_batch_2',
-        'data_batch_3',
-        'data_batch_4',
-        'data_batch_5',
-    ]
-    def __init__(self):
-        self.data = []
+    train_list = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
+    test_list = ['test_batch']
 
-        for file_name in self.train_list:
+    def __init__(self, train=True):
+        self.data = []
+        self.labels = []
+        self.downloaded_list = None
+
+        if train == True:
+            self.downloaded_list = self.train_list
+        else:
+            self.downloaded_list = self.test_list
+
+        for file_name in self.downloaded_list:
             file_path = os.path.join(os.getcwd(), "raw_data", file_name)
             with open(file_path, 'rb') as f:
                 entry = pickle.load(f, encoding='latin1')
                 self.data.append(entry['data'])
+                self.labels += entry['labels']
 
         self.data = np.vstack(self.data).reshape(-1, 3, 32, 32)
 
+    # __getitem__, __len__은 python의 규약(protocol) 일종
+    # duck typing : if it walks like a duck and it quacks like a duck, then it must be a duck.
     def __getitem__(self, index):
-        img = self.data[index]
-        return img
+        img, target = self.data[index], self.labels[index]
+        return img, target
 
     def __len__(self):
         return len(self.data)
 
 if __name__ == "__main__":
-    md = mytrainset()
+    md = mytrainset(train=True)
     mdloader = torch.utils.data.DataLoader(md, batch_size=4, shuffle=False)
     dataiter = iter(mdloader)
-    images = dataiter.next()
+    images, labels = dataiter.next()
     imshow(torchvision.utils.make_grid(images))
