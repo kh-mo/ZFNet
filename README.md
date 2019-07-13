26648a58f342642791832b409eda3a07fd66b397
mosi <momkdh2@naver.com>
2019-07-13 23:18:34 +0900

    no use visualization

--- README.md
+++ README.md
@@ -4,10 +4,6 @@ This repository is implemented by Pytorch.
 
 **ZFNet**: [Paper](https://arxiv.org/abs/1311.2901) | accepted ECCV 2014
 
-## Result
-
-## Loss
-
 ## Getting Start
 ### Download data
 - Download in current space
@@ -26,7 +22,17 @@ python check_dataset.py
 ```
 
 ### modeling
+- Input : train files
+- Output : convnet model
+```shell
+python modeling.py \
+    --batch_size = 128
+    --epochs = 50
+```
 
 ### evaluating
-
-### visualizing
+- Input : convnet model, test files
+- Output : predict category
+```shell
+python evaluating.py
+```
--- visualization.py
+++ visualization.py
@@ -1,3 +1,5 @@
+## 사용안함
+
 import os
 import torch
 from modeling import ConvNet
@@ -10,15 +12,23 @@ if __name__ == "__main__":
 
     # model & pretrain parameter load
     model = ConvNet().to(device=device)
-    model.state_dict(torch.load(os.path.join(os.getcwd(), "saved_model/convnet")))
+    model.load_state_dict(torch.load(os.path.join(os.getcwd(), "saved_model/convnet")))
 
     # test data load
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     test_loader = torch.utils.data.DataLoader(mytrainset(train=False), batch_size=1)
     dataiter = iter(test_loader)
     images, labels = dataiter.next()
 
     # visualization
-    imshow(images[0])
+    imshow(images[0]) ## torch.uint8, min 0, max 255, shape [3, 32, 32]
+    print(classes[torch.argmax(model(images.to(torch.float32).to(device=device)))])
+
+    images[0].shape
+    imshow(model.visualization(images.to(torch.float32).to(device=device), layer="layer2").detach().to(torch.uint8).to(device="cpu")[0])
+
+
+
     imshow(torch.mul(images[0].to(device), model.visualization(images.to(torch.float32).to(device=device),layer="layer2")[0].to(device).detach()))
     imshow(model.visualization(images.to(torch.float32).to(device=device),layer="layer2")[0].detach())
     imshow(model.visualization(images.to(torch.float32).to(device=device))[0].to(device).detach())
